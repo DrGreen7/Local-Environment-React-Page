ec38b8829f5b6435e76cb4622e39d5266fb9dd27
Badal <gbadal797@gmail.com>
2022-09-13 17:30:44 +0530

    created react app,and finished

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+function Main() {
+  return (
+    <div className="main">
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created bt Jordan Walke</li>
+        <li>Has well over 100k stars on GitHub</li>
+        <li>Is maintained bt Facebook</li>
+        <li>Powers thousands of entrprises apps, including mobile apps</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Main;
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import logo from "../images/React.webp";
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div>
+        <img src={logo} />
+        <h1>ReactFacts</h1>
+      </div>
+      <h1>React Course Project-1</h1>
+    </div>
+  );
+}
+
+export default Navbar;
